64fa5d989509aa252ee0abff1806e16df86512ff
Aditya Gupta <adityagupta4114@gmail.com>
2025-10-28 19:59:16 +0530

    hashmap resizing implemented

--- hashmap/src/Main.java
+++ hashmap/src/Main.java
@@ -1,18 +1,29 @@
-import java.util.ArrayList;
-import java.util.List;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        MyMap<String, Integer> m = new MyHashMap<>(10);
+        MyMap<String, Integer> m = new MyHashMap<>(8, 0.75f);
         m.put("A", 70);
         m.put("B", 180);
+        m.put("I", 1430);
+        m.put("J", 430);
         m.put("C", 130);
+        m.put("D", 130);
 
+        m.printMap();
+
+        m.put("H", 10);
         System.out.println("value for 'C': " + m.get("C"));
 
+        m.printMap();
+
         m.remove("B");
+        m.remove("J");
+        m.put("Q", 90);
+
+        m.printMap();
+
+        m.remove("A");
 
         m.printMap();
     }
@@ -24,7 +35,6 @@ public class Main {
 // remove from map
 // get from map
 // resize map
-// shrink back map
 
 interface MyMap<T, V> {
     public void put(T key, V value);
@@ -36,54 +46,75 @@ interface MyMap<T, V> {
 class MyHashMap<T, V> implements MyMap<T, V> {
 
     private int size;
-    private Node []list;
+    private Node<T, V> []list;
+    private float loadFactor;
+    private int currentSize;
 
-    MyHashMap(int size) {
-        this.size = size;
+    MyHashMap(int size, float loadFactor) {
+        this.size = getOptimalSize(size);
         this.list = new Node[size];
+        this.loadFactor = loadFactor;
+        this.currentSize = 0;
+    }
+
+    private int getOptimalSize(int size) {
+        int optimalSize = 1;
+
+        while (optimalSize < size ) {
+            optimalSize <<= 1;
+        }
+        return optimalSize;
     }
 
     @Override
     public void put(T key, V value) {
-        int hash = key.hashCode();
-        int idx = hash % this.size;
+        int idx = getIdx(key);
 
         if (this.list[idx] == null) {
             this.list[idx] = new Node<T, V>(key, value);
         } else {
             Node<T, V> node = this.list[idx];
             while (node.getNext() != null) {
+                if (node.getKey() == key) {
+                    node.setValue(value);
+                    return;
+                }
                 node = node.getNext();
             }
+            System.out.println("Key collision for key: " + key);
             node.setNext(new Node<>(key, value));
         }
+        this.currentSize++;
+        doResizeIfRequired();
     }
 
     @Override
     public V get(T key) {
-        int hash = key.hashCode();
-        int idx = hash % this.size;
+        int idx = getIdx(key);
 
         Node<T, V> node = this.list[idx];
         while (node != null) {
             if (node.getKey() == key) return node.getValue();
+            node = node.getNext();
         }
         return null;
     }
 
     @Override
     public void remove(T key) {
-        int hash = key.hashCode();
-        int idx = hash % this.size;
+        int idx = getIdx(key);
 
         Node<T, V> node = this.list[idx];
         if (node != null && node.getKey() == key) {
             this.list[idx] = node.getNext();
+            this.currentSize--;
             return;
         }
         while (node != null && node.getNext() != null) {
             if (node.getNext().getKey() == key) {
                 node.setNext(node.getNext().getNext());
+                this.currentSize--;
+                System.out.println("Key removed: " + key);
                 return;
             }
         }
@@ -91,12 +122,59 @@ class MyHashMap<T, V> implements MyMap<T, V> {
 
     @Override
     public void printMap() {
+        int i = 0;
         for (Node<T, V> node : this.list) {
+            System.out.print(i + ": ");
             while (node != null) {
-                System.out.println(node.getKey() + ": " + node.getValue());
+                System.out.print(node.getKey() + ": " + node.getValue() + " ");
                 node = node.getNext();
             }
+            System.out.println();
+            i++;
         }
+        System.out.println("\n");
+    }
+
+    private int getIdx(T key) {
+        int hash = key.hashCode();
+        return hash & (this.size - 1);
+    }
+
+    private void doResizeIfRequired() {
+        float currentLoad = (float) this.currentSize / this.size;
+        if (currentLoad > this.loadFactor) {
+            triggerResize();
+        }
+    }
+
+    private void triggerResize() {
+        System.out.println("Hashmap resize triggered!!");
+        this.size <<= 1;
+        Node<T, V> []newList = new Node[this.size];
+
+        for (int i = 0 ; i<this.list.length ; i++) {
+            Node<T, V> node = this.list[i];
+            if (node == null) continue;
+
+            while (node != null) {
+                T key = node.getKey();
+                int newIndex = getIdx(key);
+
+                if (newList[newIndex] == null) newList[newIndex] = node;
+                else {
+                    Node<T, V> tempNode = newList[newIndex];
+                    while (tempNode.getNext() != null) {
+                        tempNode = tempNode.getNext();
+                    }
+                    tempNode.setNext(node);
+                }
+                Node<T, V> nextNode = node.getNext();
+                node.setNext(null);
+                node = nextNode;
+            }
+        }
+
+        this.list = newList;
     }
 
 
